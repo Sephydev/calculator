30aa5877c47361b475643be0b101f6b932a7bd42
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-07 16:12:37 +0100

    Fix bug when click "=" without choosing operator

--- script.js
+++ script.js
@@ -52,7 +52,11 @@ let result;
 
 digits.forEach(digit => {
     digit.addEventListener("click", (e) => {
+
+        // Code works only when result is not calculated
         if (result === undefined) {
+
+            // Code for choosing first number
             if (operatorValue === undefined) {
                 if (num1 === 0) {
                     num1 = e.target.value;
@@ -60,6 +64,8 @@ digits.forEach(digit => {
                     num1 += e.target.value;
                 }
                 display.textContent = num1;
+
+                //Code for choosing second number
             } else {
                 if (num2 === undefined) {
                     num2 = e.target.value;
@@ -74,15 +80,20 @@ digits.forEach(digit => {
 
 operators.forEach(operator => {
     operator.addEventListener("click", (e) => {
+
+        // Code for selecting operator, work if num2 is not been chosen and operator is not "="
         if (e.target.value !== "=" && num2 === undefined) {
+
+            // Code for replacing operator in case of wrong operator chosen
             if (operatorValue !== undefined) {
-                console.log("test")
                 display.textContent = display.textContent.substring(0, display.textContent.length - 1);
             }
             console.log(operatorValue);
             operatorValue = e.target.value;
             display.textContent += " " + operatorValue;
-        } else if (e.target.value === "=") {
+
+            // Code for calculating result.
+        } else if (e.target.value === "=" && num2 !== undefined && operatorValue !== undefined) {
             result = operate(+num1, +num2, operatorValue);
             display.textContent += ` ${e.target.value} ${result}`;
         }
