614886bca4d04e798fae44663307ed838980d8b6
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-07 14:36:20 +0100

    Add function "divide"

--- script.js
+++ script.js
@@ -10,4 +10,11 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-console.log(multiply(8, 5));
+function divide(num1, num2) {
+    if (num2 !== 0) {
+        return num1 / num2;
+    } else {
+        return "Error";
+    }
+}
+console.log(divide(8, 5));
