e45f1d0014033ad16f7c8815e4a1574d952dbc3e
Sephydev <nicolas.bussiere@outlook.fr>
2025-01-07 21:07:03 +0100

    Add keyboard support

--- script.js
+++ script.js
@@ -39,110 +39,14 @@ function operate(num1, num2, operator) {
     return result;
 }
 
-const digits = document.querySelectorAll(".digit");
-const operators = document.querySelectorAll(".operator");
-const display = document.querySelector("#display");
-const clear = document.querySelector("#clear");
-const backspace = document.querySelector("#backspace");
+function calculate(operator) {
+    if (operator === "=" && num2 !== "" && operatorValue !== "") {
+        result = operate(+num1, +num2, operatorValue);
+        display.textContent += ` ${operator} ${result}`;
+    }
+}
 
-let num1 = "";
-let operatorValue = "";
-let num2 = "";
-let result = "";
-
-digits.forEach(digit => {
-    digit.addEventListener("click", (e) => {
-
-        // Stop user to write number after result (ex: 1 + 1 = 15'553')
-        if (result === "") {
-
-            // User can enter first number before choosing operator
-            if (operatorValue === "") {
-                if (e.target.value === ".") {
-                    if (!(num1.includes("."))) {
-                        num1 += e.target.value;
-                    }
-                } else {
-                    if (num1 === "" || num1 === "0") num1 = e.target.value;
-                    else num1 += e.target.value
-                }
-
-                display.textContent = num1;
-
-                // User can enter second number after choosing operator
-            } else {
-                if (e.target.value === ".") {
-                    if (!(num2.includes("."))) {
-                        num2 += e.target.value;
-                        display.textContent += e.target.value;
-                    }
-                } else {
-                    if (num2 === "" || num2 === "0") {
-                        num2 = e.target.value;
-                    } else {
-                        num2 += e.target.value;
-                    }
-
-                    if (display.textContent.charAt(display.textContent.length - 1) === "0") {
-                        display.textContent = display.textContent.substring(0, display.textContent.length - 1);
-                    }
-
-                    display.textContent += e.target.value;
-                }
-
-
-            }
-        }
-    })
-})
-
-operators.forEach(operator => {
-    operator.addEventListener("click", (e) => {
-
-        // User can choose operator (but not "=") if num1 contains a value and num2 doesn't contain a value
-        if (e.target.value !== "=" && num2 === "" && num1 !== "") {
-
-            // User can modify the operator if he didn't enter a digit for num2
-            if (operatorValue !== "") {
-                display.textContent = display.textContent.substring(0, display.textContent.length - 2);
-            }
-            operatorValue = e.target.value;
-            display.textContent += " " + operatorValue + " ";
-
-            // Stop the "divide by zero" error.
-        } else if (operatorValue === "/" && num2 === "0") {
-            display.textContent = "Cannot divide by zero!";
-            operatorValue = "";
-            num1 = "";
-            num2 = "";
-
-            // User can chain operations with result being num1
-        } else if ((result !== "" && e.target.value !== "=")) {
-            num1 = `${result}`;
-            num2 = "";
-            result = "";
-            operatorValue = e.target.value;
-
-            display.textContent = `${num1} ${operatorValue} `;
-
-            // The operation is calculated
-        } else if (e.target.value === "=" && num2 !== "" && operatorValue !== "") {
-            result = operate(+num1, +num2, operatorValue);
-            display.textContent += ` ${e.target.value} ${result}`;
-        }
-    })
-})
-
-clear.addEventListener("click", () => {
-    num1 = "";
-    num2 = "";
-    operatorValue = "";
-    result = "";
-
-    display.textContent = "0";
-})
-
-backspace.addEventListener("click", () => {
+function backSpace() {
     if (result === "") {
         if (num2 !== "") {
             num2 = num2.substring(0, num2.length - 1);
@@ -162,4 +66,128 @@ backspace.addEventListener("click", () => {
             }
         }
     }
+}
+
+function inputDigits(digit) {
+    if (result === "") {
+        if (operatorValue === "") {
+            if (digit === ".") {
+                if (!(num1.includes("."))) {
+                    num1 += digit;
+                }
+            } else {
+                if (num1 === "" || num1 === "0") num1 = digit;
+                else num1 += digit
+            }
+            display.textContent = num1;
+        } else {
+            if (digit === ".") {
+                if (!(num2.includes("."))) {
+                    num2 += digit;
+                    display.textContent += digit;
+                }
+            } else {
+                if (num2 === "" || num2 === "0") {
+                    num2 = digit;
+                } else {
+                    num2 += digit;
+                }
+
+                if (display.textContent.charAt(display.textContent.length - 1) === "0") {
+                    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+                }
+
+                display.textContent += digit;
+            }
+        }
+    }
+}
+
+function inputOperator(operator) {
+    if (operator !== "=" && num2 === "" && num1 !== "") {
+
+        if (operatorValue !== "") {
+            display.textContent = display.textContent.substring(0, display.textContent.length - 2);
+        }
+        operatorValue = operator;
+        display.textContent += " " + operatorValue + " ";
+
+    } else if (operatorValue === "/" && num2 === "0") {
+        display.textContent = "Cannot divide by zero!";
+        operatorValue = "";
+        num1 = "";
+        num2 = "";
+
+    } else if ((result !== "" && operator !== "=")) {
+        num1 = `${result}`;
+        num2 = "";
+        result = "";
+        operatorValue = operator;
+
+        display.textContent = `${num1} ${operatorValue} `;
+    }
+}
+
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const display = document.querySelector("#display");
+const clear = document.querySelector("#clear");
+const backspace = document.querySelector("#backspace");
+
+let num1 = "";
+let operatorValue = "";
+let num2 = "";
+let result = "";
+
+digits.forEach(digit => {
+    digit.addEventListener("click", (e) => inputDigits(e.target.value))
 })
+
+operators.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        inputOperator(e.target.value);
+        calculate(e.target.value);
+    })
+})
+
+clear.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    operatorValue = "";
+    result = "";
+
+    display.textContent = "0";
+})
+
+backspace.addEventListener("click", () => backSpace());
+
+document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "Backspace":
+            backSpace();
+            break;
+        case "Enter":
+        case "=":
+            calculate("=");
+            break;
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case ".":
+            inputDigits(e.key);
+            break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            inputOperator(e.key);
+            break;
+    }
+});
